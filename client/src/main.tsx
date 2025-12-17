aaaab634ffb574e04895acbd3cc40c79c8494356
ajithyaasaf <ajithkannan.ag@gmail.com>
2025-12-17 18:40:13 +0530

    Add index.css and update main.tsx
    - Added new index.css file for global styles
    - Updated main.tsx configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot, hydrateRoot } from "react-dom/client";
 import App from "./App";
 import "./critical.css";
+import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { TooltipProvider } from "@/components/ui/tooltip";
